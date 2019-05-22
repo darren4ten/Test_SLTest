3e5cc3ffc1ba54171ae4a745d53ac01a09260c50
Darren Teng <darren.teng@greendotcorp.com>
2019-05-22 14:20:59 +0800

    add test sln

---
+++ SourceLinkTest/Speaker.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SourceLinkTest
+{
+    public class Speaker
+    {
+        public void Speak(String name, String msg)
+        {
+            Console.WriteLine($"Hello {name}, {msg} {DateTime.Now}");
+        }
+    }
+}
